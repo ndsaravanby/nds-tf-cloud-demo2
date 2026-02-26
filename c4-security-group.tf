7ae2ba4545045d71bac1d2c2368f81d6a08216e7
ndsravanby <ndsaravanby@gmail.com>
2026-02-26 08:40:48 +0530

    updated-icmp-port-connectivity

--- c4-security-group.tf
+++ c4-security-group.tf
@@ -10,6 +10,20 @@ ingress {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
 }
+
+ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+}
+
+ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+}
 egress {
 
     from_port = 0
